5bc966bf5e8c80c8118022654dc9d05cb57b6513
navidshh <navid.shirzadi@nrcan-rncan.gc.ca>
2024-06-28 17:09:58 +0000

    test file updated

--- test/necb/unit_tests/tests/test_shw.rb
+++ test/necb/unit_tests/tests/test_shw.rb
@@ -4,7 +4,6 @@ include(NecbHelper)
 
 class NECB_BTAP_Data_Reporting < Minitest::Test
 
-
   def setup()
     define_folders(__dir__)
     define_std_ranges
@@ -15,6 +14,7 @@ class NECB_BTAP_Data_Reporting < Minitest::Test
 
     # Set up remaining parameters for test.
     output_folder = method_output_folder(__method__)
+    expected_results_folder = "#{File.dirname(__FILE__)}/../expected_results/"
     template = 'NECB2011'
     standard = get_standard(template)
     save_intermediate_models = true
@@ -24,42 +24,25 @@ class NECB_BTAP_Data_Reporting < Minitest::Test
     primary_heating_fuel = 'NaturalGas'
     shw_fuel = 'FuelOilNo2'
     epw_file = 'CAN_ON_Toronto.Intl.AP.716240_CWEC2020.epw'
-    # Generate osm file.
-    model = standard.model_create_prototype_model(building_type: building_type,
-                                                  epw_file: epw_file,
-                                                  template: template,
-                                                  primary_heating_fuel: primary_heating_fuel,
-                                                  shw_fuel: shw_fuel,
-                                                  sizing_run_dir: output_folder)
 
-    # standard.model_run_simulation_and_log_errors(model, output_folder)
-    # Create the results file
-    # qaqc = standard.init_qaqc(model)
-    # # Replace the openstudio-standards version with test to avoid the test failing with every commit to a branch.
-    # qaqc[:os_standards_revision] = "test"
-    # qaqc[:os_standards_version] = "test"
-    # qaqc[:openstudio_version] = "test"
-    # qaqc[:energyplus_version] = "test"
-    # btap_data_out = BTAPData.new(model: model,
-    #                           runner: nil,
-    #                           cost_result: nil,
-    #                           qaqc: qaqc,
-    #                           npv_start_year: 2010,
-    #                           npv_end_year: 2030,
-    #                           npv_discount_rate: @npv_discount_rate).btap_data
-    # #btap_data_out.select.first
-    # btap_data_out["simulation_btap_data_version"] = "test"
-    # btap_data_out["simulation_os_standards_revision"] = "test"
-    # btap_data_out["simulation_os_standards_version"] = "test"
-    # btap_data_out["simulation_date"] = "test"
-    btap_data_expected_results = File.join(@expected_results_folder, 'btap_data_report_expected_result.json')
-    btap_data_test_results = File.join(@test_results_folder, 'btap_data_report_test_result.json')
-    unless File.exist?(btap_data_expected_results)
-      puts("No expected results file, creating one based on test results")
-      File.write(btap_data_expected_results, JSON.pretty_generate(btap_data_out))
+    # Generate osm file.
+    model = standard.build(building_type: building_type,
+                           epw_file: epw_file,
+                           template: template,
+                           primary_heating_fuel: primary_heating_fuel,
+                           shw_fuel: shw_fuel,
+                           sizing_run_dir: output_folder)
+
+    model_name = "#{building_type}-#{template}-#{primary_heating_fuel}-#{shw_fuel}-#{File.basename(epw_file, '.epw')}"
+    test_osm_file = "#{expected_results_folder}#{model_name}_test_result.osm"
+
+    if !model.instance_of?(OpenStudio::Model::Model)
+      puts "Creation of Model for #{model_name} failed. Please check output for errors."
+      assert(false, "Model creation failed")
+    else
+      # Save the model to the expected results folder
+      BTAP::FileIO.save_osm(model, test_osm_file)
+      puts "saved test result osm file to #{test_osm_file}"
     end
-    File.write(btap_data_test_results, JSON.pretty_generate(btap_data_out))
-    msg = "The btap_data_report_test_results.json differs from the btap_data_report_expected_results.json.  Please review the results."
-    file_compare(expected_results_file: btap_data_expected_results, test_results_file: btap_data_test_results, msg: msg)
   end
 end
